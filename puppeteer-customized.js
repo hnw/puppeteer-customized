53e08ba16d557ba2732953b649e92c82c0b17a03
Yoshio HANAWA <y@hnw.jp>
2019-07-07 16:15:57 +0900

    ElementHandleにisVisibleメソッドを追加

--- puppeteer-customized.js
+++ puppeteer-customized.js
@@ -1,12 +1,22 @@
 'use strict'
 const puppeteer = require('puppeteer');
 const myPuppeteer = Object.assign({}, puppeteer);
+const {ElementHandle} = require('puppeteer/lib/api');
+
+ElementHandle.prototype.isVisible = async function () {
+  return await this.executionContext().evaluate(el => {
+    if (!el)
+      return false;
+    const style = window.getComputedStyle(el);
+    return style && style.display !== 'none' && style.visibility !== 'hidden' && style.opacity !== '0';
+  }, this);
+};
 
 // private variables
 let defaults = {};
 
 // add new method setDefaults()
-myPuppeteer.setDefaults = function(options = {}) {
+myPuppeteer.setDefaults = function (options = {}) {
   /* some custom behavior */
   Object.entries(options).forEach(([k, v]) => {
     const camelCase = k.replace(/([-_]+[a-z0-9])/ig, ($1) => {
@@ -27,7 +37,7 @@ myPuppeteer.setDefaults = function(options = {}) {
 
 // replace puppeteer.launch()
 const origLaunch = myPuppeteer.launch;
-myPuppeteer.launch = function(options = {}) {
+myPuppeteer.launch = function (options = {}) {
   const merged = Object.assign({}, defaults, options);
   const browser = origLaunch.apply(myPuppeteer, [merged]);
   return browser;
