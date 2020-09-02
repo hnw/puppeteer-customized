6cadc731aaf29264b1ffc83800ea1b14b9198168
Yoshio HANAWA <y@hnw.jp>
2020-09-03 05:02:03 +0900

    Puppeteerの最新バージョンでElementHandleその他モジュールの上書きができなくなっていたので便利メソッドを削除

--- puppeteer-customized.js
+++ puppeteer-customized.js
@@ -1,70 +1,9 @@
 'use strict'
 const puppeteer = require('puppeteer');
+
 // オブジェクトをclone
 // via: https://stackoverflow.com/questions/41474986/how-to-clone-a-javascript-es6-class-instance
 const myPuppeteer = Object.assign(Object.create(Object.getPrototypeOf(puppeteer)), puppeteer);
-const {ElementHandle, Page} = require('puppeteer/lib/api');
-
-ElementHandle.prototype.isVisible = async function () {
-  return this.boundingBox().then(obj => obj !== null);
-}
-
-Object.defineProperty(ElementHandle.prototype, 'visibleText', {
-  async get() {
-    return Promise.all(
-      (await this.$x('descendant::text()')).map(async el => {
-        if (!await el.isVisible()) {
-          return '';
-        };
-        return el.evaluate(node => node.textContent.trim().replace(/\s+/g,' '));
-      })
-    ).then(texts => texts.filter(Boolean).join(' '));
-  }
-});
-
-Object.defineProperty(ElementHandle.prototype, 'textContent', {
-  async get() {
-    return (await this.getProperty('textContent')).jsonValue();
-  }
-});
-
-Object.defineProperty(ElementHandle.prototype, 'innerText', {
-  async get() {
-    return (await this.getProperty('innerText')).jsonValue();
-  }
-});
-
-Object.defineProperty(ElementHandle.prototype, 'innerHTML', {
-  async get() {
-    return (await this.getProperty('innerHTML')).jsonValue();
-  }
-});
-
-Object.defineProperty(ElementHandle.prototype, 'outerText', {
-  async get() {
-    return (await this.getProperty('outerText')).jsonValue();
-  }
-});
-
-Object.defineProperty(ElementHandle.prototype, 'outerHTML', {
-  async get() {
-    return (await this.getProperty('outerHTML')).jsonValue();
-  }
-});
-
-Object.defineProperty(ElementHandle.prototype, 'value', {
-  async get() {
-    return (await this.getProperty('value')).jsonValue();
-  }
-});
-
-Page.prototype.waitForSelectors = async function (selectors, options) {
-  if (!Array.isArray(selectors)) {
-    return this.waitForSelector(selectors, options);
-  }
-  const ret = await this.waitForSelector(selectors.join(','), options);
-  return Promise.all(selectors.map(async (selector) => this.$(selector)));
-}
 
 // private variables
 let defaults = {};
